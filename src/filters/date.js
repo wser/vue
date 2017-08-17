455c63fc92a2aa002a6baa0bb11315764dfe8c5b
wser <w.web.dev@gmail.com>
2017-08-17 15:32:54 +0200

    setted signup process

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,4 +1,9 @@
 export default (value) => {
 	const date = new Date(value)
-	return date.toLocaleString(['en-US'], {month: 'short', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})
+	return date.toLocaleString(['en-US'], {
+		month: 'short',
+		year: 'numeric',
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit'})
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDfUOh731L-D23iMoHSQC92q_trxhWEqLY',
+      authDomain: 'yt-dev-meetup.firebaseapp.com',
+      databaseURL: 'https://yt-dev-meetup.firebaseio.com',
+      projectId: 'yt-dev-meetup',
+      storageBucket: 'yt-dev-meetup.appspot.com'
+    })
+  }
 })
